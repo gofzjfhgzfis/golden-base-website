26060b9a1922ba8e7eb98d651cfdd36da2c9c21b
Yusf <yusfsalah58@gmail.com>
2026-08-22 22:06:45 +0300

    Configure for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ بۆ GitHub Pages: base دەبێت ناوی ڕیپۆزیتۆری بێت.
-  //    کاتێک دەیبەیتە Netlify، ئەمە بگۆڕە بۆ '/'
-  base: '/golden-base-website/',
+  // Netlify → base دەبێت '/' بێت
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
